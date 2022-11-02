69607831ab9aa1be77d5a6570726996d83298886
Sidharth-S-Kumar <77217570+Sidharth-S-Kumar@users.noreply.github.com>
2022-11-01 22:07:01 -0700

    Problem 2 Added

--- Problem2Code.m
+++ Problem2Code.m
@@ -1,23 +1,27 @@
 %ME3492 Project 3 Kumar, Sidharth, Thomas,Mathew
 %Constants/Givens/assumptions
-rho = 1.69;%slug/ft^3
-mu = 2.87E-5 ; %lbf*s/ft
+SG= 0.823; 
+mu= 1.64E-3;
+rho = SG*1000;%kg/m^3
 e= 1.5E-4;
-Qtot = 100; %GPM
-Qtot = Qtot*0.0022280092365745; %CFS
-D1 = .1723; %ft
-D2 = .1342; %ft
-L1 = 150; %ft
-L2 = 125; %ft
-K1 = 12.1; 
-K2 = 2.8; 
-Pb = 10; %psig
+Qtot = 2; %L/s
+Qtot = Qtot*0.001; %CMS
+D1 = 2.604E-2; %m
+D2 = 1.994E-2; %m
+L1 = 50; %m
+L2 = 30; %m
+K1 = 10+6*.31; 
+K2 = 70+2*.31; 
+Pb = 200; %kpa
 A1 = (pi*D1^2)/4;
 A2 = (pi*D2^2)/4 ;
 Q1= Qtot*(A1/(A1+A2));
 Qtotnew = 0; 
-while(Qtotnew<=.99*Qtot||Qtotnew>=1.01*Qtot)
+count = 0;
+%Qtotnew<=.999*Qtot||Qtotnew>=1.001*Qtot
+while(Qtotnew<=.9999*Qtot||Qtotnew>=1.0001*Qtot)
 %Flow Rate initial assumption 
+count = count +1; 
 Q2 = Qtot-Q1; 
 
 V1 = Q1/A1;
@@ -35,8 +39,8 @@ V2 = (2*p*rho^-1*((f2*L2*D2^-1)+K2)^-1)^.5;
 
 Q2 = A2*V2;
 Qtotnew = Q1+Q2;
-if(Qtotnew<=.99*Qtot||Qtotnew>=1.01*Qtot)
+if(Qtotnew<=.9999*Qtot||Qtotnew>=1.0001*Qtot)
+    Q1= (Q1/Qtotnew)*Qtot;
 end
-Q1= (Q1/Qtotnew)*Qtot;
 end 
 Pa = Pb + p; 
